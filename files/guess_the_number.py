aa5b70fc7025864e9bb3133362b3fdd587b6d3f8
Mihau91 <pierdoot@gmail.com>
2022-07-02 10:15:38 +0200

    while loop added in main function to let user play until he wins

--- files/guess_the_number.py
+++ files/guess_the_number.py
@@ -26,12 +26,14 @@ def guess_the_number():
     print("""Welcome to "Guess The Number" game. Your task is to guess the number from 1 - 100. Good luck!""")
     given_number = get_number()
     secret_number = randint(1, 100)
-    if given_number == secret_number:
-        print("You win!")
-    elif given_number < secret_number:
-        print("Your number is too small. Try again.")
-    else:
-        print("Your number is too big. Try again.")
+
+    while given_number != secret_number:
+        if given_number < secret_number:
+            print("Your number is too small. Try again.")
+        else:
+            print("Your number is too big. Try again.")
+        given_number = get_number()
+    print("Congratulations! You win!")
 
 
 if __name__ == '__main__':
