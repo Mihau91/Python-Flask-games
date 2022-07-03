6f845ab99e1702ab84a108cb01e90d1b29aa2aab
Mihau91 <pierdoot@gmail.com>
2022-07-03 14:14:37 +0200

    prepared welcome page, function render html depends on POST or GET method

--- files/guessnum_flask.py
+++ files/guessnum_flask.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def guess_the_number():
     if request.method == "GET":
+        min_value = 0
+        max_value = 1000
+        return render_template("welcome_page.html", min=min_value, max=max_value)
+    else:
         return render_template("main_page.html")
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
